55950f0d3d0993c8aa2ee5132baea9a932157651
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-04 20:03:40 -0700

    Remove unnecessary React logos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './generated/tailwind.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <p className="text-green-500">This will eventually be our wedding website! Please ignore how it looks at the moment</p>
       </header>
     </div>
