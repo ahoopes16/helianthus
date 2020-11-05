bda567e68881afb33724d725209333ad11eb9f27
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-04 22:25:23 -0700

    Create tailwind config
    * https://tailwindcss.com/docs/configuration#app

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#fff9f0',
+        navy: '#1C2544',
+      },
+      fontSize: {
+        '8xl': '6rem',
+      },
+      scale: {
+        'flip': '-1',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
