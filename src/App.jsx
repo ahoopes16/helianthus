e7c6baed43372fdff626b67ba1295956a5994d0d
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-04 23:47:22 -0700

    Extract size and margin styles to variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import './generated/tailwind.css'
 import sunflower from './img/sunflower.png'
+import MediaQuery from 'react-responsive'
+
+const sunflowerStyles = {
+  sizes: `xl:h-88`,
+  margins: `xl:-mt-20`,
+}
+
+const headingStyles = {
+  sizes: `xl:text-6xl`,
+  margins: `xl:my-8`,
+}
+
+const paragraphStyles = {
+  sizes: `xl:text-4xl`
+}
+
+const hashtagStyles = {
+  sizes: `xl:text-5xl`,
+  margins: `xl:mt-24`
+}
 
 function App() {
   const appClass = `h-full w-full bg-cream
                     flex flex-col
-                    text-center text-navy text-roboto text-4xl`
+                    text-center text-navy text-roboto ${paragraphStyles.sizes}`
 
   return (
     <div className={appClass}>
-      <h1 className="text-parisienne text-8xl my-8">Nikki & Alex's Wedding Website</h1>
+      <h1 className={`text-parisienne ${headingStyles.sizes} ${headingStyles.margins}`}>Nikki & Alex's Wedding Website</h1>
 
       <div className="my-6">
         <p>Under Construction Until April 2021</p>
@@ -16,21 +36,22 @@ function App() {
       </div>
 
       <div className='flex flex-row justify-around my-24'>
+        <MediaQuery minDeviceWidth={1280}>
+          <img
+            src={sunflower}
+            alt="left sunflower graphic"
+            className={`object-contain ${sunflowerStyles.sizes} ${sunflowerStyles.margins} xl:-mr-32 transform -rotate-45`}
+          />
+        </MediaQuery>
+
+        <h2 className={`${hashtagStyles.sizes} ${hashtagStyles.margins}`}>#2haveand2hoopes</h2>
+
         <img
           src={sunflower}
-          alt="sunflower graphic"
-          className="object-contain h-3/4 ml-24 -mt-20 transform -rotate-45 "
-        />
-
-        <h2 className="text-6xl pt-24">#2haveand2hoopes</h2>
-
-        <img
-          src={sunflower}
-          alt="sunflower graphic"
-          className="object-contain h-3/4 mr-24 -mt-20 transform scale-x-flip rotate-45"
+          alt="right sunflower graphic"
+          className={`object-contain ${sunflowerStyles.sizes} ${sunflowerStyles.margins} xl:-ml-32  transform scale-x-flip rotate-45`}
         />
       </div>
-
     </div>
   );
 }
