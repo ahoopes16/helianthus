0d9acf3e30aa713eb589656c0c93a677e65e8cb6
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2020-11-04 22:27:06 -0700

    Add all text content and lay it out properly

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
 
   return (
     <div className={appClass}>
+      <h1 className="text-parisienne text-8xl my-8">Nikki & Alex's Wedding Website</h1>
+
+      <div className="my-6">
+        <p>Under Construction Until April 2021</p>
+        <p>Come back then to RSVP and receive all wedding details!</p>
+      </div>
+
+      <div className='flex flex-row justify-around my-24'>
+        <h2 className="text-6xl pt-24">#2haveand2hoopes</h2>
+      </div>
+
     </div>
   );
 }
