8a03d9ffac361cb3a32979ae965fe5519de0aab1
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-18 16:02:04 -0700

    Create placeholder content pages to test routing

---
+++ src/content/Gifts.jsx
@@ -0,0 +1,5 @@
+function Gifts() {
+    return <h1 className='text-center text-cabernet text-parisienne underline text-5xl'>Gifts</h1>
+}
+
+export default Gifts
---
+++ src/content/Lodging.jsx
@@ -0,0 +1,5 @@
+function Lodging() {
+    return <h1 className='text-center text-cabernet text-parisienne underline text-5xl'>Lodging</h1>
+}
+
+export default Lodging
