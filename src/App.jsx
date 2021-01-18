aeaf441c33b55b7d834cbf08ad9709b6a19edc2a
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-18 16:02:25 -0700

    Add React Router elements to go to each page with a given route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import './generated/tailwind.css'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { ContentCard, ContentWrapper, Header, Navigation } from './components'
+import { About, RSVP, FAQ, Travel, Lodging, PointsOfInterest, EngagementPhotos, Gifts } from './content'
 
 function App() {
   return (
-    <div id='app' className='bg-steel text-gold'>
-      <Header />
+    <HashRouter>
+      <div id='app' className='bg-steel text-gold'>
+        <Header />
 
-      <ContentWrapper>
-        <Navigation />
-        <ContentCard>
-          <h1 className='text-center text-cabernet text-parisienne underline text-5xl'>About</h1>
-        </ContentCard>
-      </ContentWrapper>
-    </div>
+        <ContentWrapper>
+          <Navigation />
+          <ContentCard>
+            <Switch>
+              <Route exact path='/about' component={About} />
+              <Route exact path='/rsvp' component={RSVP} />
+              <Route exact path='/faq' component={FAQ} />
+              <Route exact path='/travel' component={Travel} />
+              <Route exact path='/lodging' component={Lodging} />
+              <Route exact path='/points-of-interest' component={PointsOfInterest} />
+              <Route exact path='/engagement-photos' component={EngagementPhotos} />
+              <Route exact path='/gifts' component={Gifts} />
+              <Redirect to='/about' />
+            </Switch>
+          </ContentCard>
+        </ContentWrapper>
+      </div>
+    </HashRouter>
   )
 }
 
