bf97faa7857d45c63ed9ebd7bbd6f7d2ce0c006c
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-18 02:23:45 -0700

    Create full-height navbar and basic content area

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,37 @@ import './generated/tailwind.css'
 import logo from './img/logo.png'
 
 function App() {
-  const appClass = 'h-screen w-screen bg-steel'
+  const appClass = 'bg-steel'
 
   return (
-    <div className={appClass}>
-      <div className='w-screen bg-navy text-gold text-parisienne text-5xl grid grid-cols-10'>
+    <div id='app' className={appClass}>
+      <div className='bg-navy text-gold text-parisienne text-5xl grid grid-cols-12'>
         <div className='flex items-center justify-center'>
           <img className='h-20 w-20' src={logo} alt='Logo' />
         </div>
-        <div className='col-span-8'>
+        <div className='col-span-11'>
           <h1 className='text-center py-4'>Nikki &#38; Alex &#8226; October 8, 2021</h1>
         </div>
       </div>
 
-      <div className='h-screen text-gold text-roboto font-bold text-xl grid grid-cols-10'>
-        <div className='text-center bg-navy'>
+      <div className='text-gold text-roboto font-bold text-2xl grid grid-cols-12'>
+        <div className='bg-navy text-center'>
           <ul className='pt-8'>
-            <li className='py-4'>About</li>
-            <li className='py-4'>RSVP</li>
-            <li className='py-4'>FAQ</li>
-            <li className='py-4'>Travel</li>
-            <li className='py-4'>Lodging</li>
-            <li className='py-4'>Points of Interest</li>
-            <li className='py-4'>Engagement Photos</li>
-            <li className='py-4'>Gifts</li>
+            <li className='py-5'>About</li>
+            <li className='py-5'>RSVP</li>
+            <li className='py-5'>FAQ</li>
+            <li className='py-5'>Travel</li>
+            <li className='py-5'>Lodging</li>
+            <li className='py-5'>Points of Interest</li>
+            <li className='py-5'>Engagement Photos</li>
+            <li className='py-5'>Gifts</li>
           </ul>
+        </div>
+
+        <div className='col-span-11 flex items-center justify-center'>
+          <div className='h-224 w-320 bg-cream shadow-xl'>
+            <p>hello</p>
+          </div>
         </div>
       </div>
     </div>
