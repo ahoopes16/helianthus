c7c426294e1624ba11cd439b85959388a9472d78
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-18 21:05:19 -0700

    Extract page routing information to a single array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './generated/tailwind.css'
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { ContentCard, ContentWrapper, Header, Navigation } from './components'
-import { About, RSVP, FAQ, Travel, Lodging, PointsOfInterest, EngagementPhotos, Gifts } from './content'
+import { pages } from './constants'
 
 function App() {
   return (
@@ -13,14 +13,7 @@ function App() {
           <Navigation />
           <ContentCard>
             <Switch>
-              <Route exact path='/about' component={About} />
-              <Route exact path='/rsvp' component={RSVP} />
-              <Route exact path='/faq' component={FAQ} />
-              <Route exact path='/travel' component={Travel} />
-              <Route exact path='/lodging' component={Lodging} />
-              <Route exact path='/points-of-interest' component={PointsOfInterest} />
-              <Route exact path='/engagement-photos' component={EngagementPhotos} />
-              <Route exact path='/gifts' component={Gifts} />
+              {pages.map(page => <Route exact path={page.path} component={page.component} />)}
               <Redirect to='/about' />
             </Switch>
           </ContentCard>
