0bbd27ec96bf725ecfe01a3cadc27b8e00373895
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-18 21:05:19 -0700

    Extract page routing information to a single array of objects

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,13 @@
 import { NavLink } from 'react-router-dom'
+import { pages } from '../constants'
 
 function Navigation() {
-    const linkClass = 'py-5'
+    const linkClass = 'py-5 hover:underline'
 
     return (
         <div className='bg-navy text-center text-2xl font-bold'>
             <div className='flex flex-col pt-8'>
-                <NavLink className={linkClass} to='/about'>About</NavLink>
-                <NavLink className={linkClass} to='/rsvp'>RSVP</NavLink>
-                <NavLink className={linkClass} to='/faq'>FAQ</NavLink>
-                <NavLink className={linkClass} to='/travel'>Travel</NavLink>
-                <NavLink className={linkClass} to='/lodging'>Lodging</NavLink>
-                <NavLink className={linkClass} to='/points-of-interest'>Points of Interest</NavLink>
-                <NavLink className={linkClass} to='/engagement-photos'>Engagement Photos</NavLink>
-                <NavLink className={linkClass} to='/gifts'>Gifts</NavLink>
+                {pages.map(page => <div className={linkClass}><NavLink to={page.path}>{page.title}</NavLink></div>)}
             </div>
         </div>
     )
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+import { About, RSVP, FAQ, Travel, Lodging, PointsOfInterest, EngagementPhotos, Gifts } from './content'
+
+export const pages = [
+    { path: '/about', title: 'About', component: About },
+    { path: '/rsvp', title: 'RSVP', component: RSVP },
+    { path: '/faq', title: 'FAQ', component: FAQ },
+    { path: '/travel', title: 'Travel', component: Travel },
+    { path: '/lodging', title: 'Lodging', component: Lodging },
+    { path: '/points-of-interest', title: 'Points of Interest', component: PointsOfInterest },
+    { path: '/engagement-photos', title: 'Engagement Photos', component: EngagementPhotos },
+    { path: '/gifts', title: 'Gifts', component: Gifts },
+]
