857497c07e5f9c2e873f572ee67c0b22676a7a4e
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-18 21:29:31 -0700

    Add class to highlight the currently selected page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,25 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { pages } from '../constants'
 
 function Navigation() {
-    const linkClass = 'py-5 hover:underline'
+    const linkContainerBaseClass = 'py-5'
+    const linkClass = 'hover:underline'
+    const selectedClass = 'text-navy bg-gold'
+    const location = useLocation()
 
     return (
         <div className='bg-navy text-center text-2xl font-bold'>
             <div className='flex flex-col pt-8'>
-                {pages.map(page => <div className={linkClass}><NavLink to={page.path}>{page.title}</NavLink></div>)}
+                {pages.map(page => {
+                    const linkContainerClass = `${linkContainerBaseClass} ${location.pathname === page.path ? selectedClass : ''}`
+
+                    return (
+                        <div key={`nav-${page.title.toLowerCase().replace(' ', '-')}`} className={linkContainerClass}>
+                            <NavLink className={linkClass} to={page.path}>{page.title}</NavLink>
+                        </div>
+                    )
+                }
+                )}
             </div>
         </div>
     )
