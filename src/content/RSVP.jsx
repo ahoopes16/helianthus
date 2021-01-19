f34bbdcf4b41db9b8811616f893af96f031fc33d
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-19 11:42:32 -0700

    Fill out RSVP page with placeholder content

--- src/content/RSVP.jsx
+++ src/content/RSVP.jsx
@@ -1,5 +1,17 @@
+import { Fragment } from 'react'
+
 function RSVP() {
-    return <h1 className='text-center text-cabernet text-parisienne text-6xl'>R.S.V.P.</h1>
+    return (
+        <Fragment>
+            <h1 className='text-center text-cabernet text-parisienne text-6xl'>R.S.V.P.</h1>
+
+            <div className='text-black text-2xl text-center mt-20 px-5'>
+                <h2 className='text-4xl text-cabernet mb-8'>This feature will be coming in a future version of the site!</h2>
+                <p className='mb-4'>This is by far the most complex part of the website, but I wanted the wedding information to be available as soon as possible.</p>
+                <p>You will be able to RSVP by the end of April, as promised. :)</p>
+            </div>
+        </Fragment>
+    )
 }
 
 export default RSVP
