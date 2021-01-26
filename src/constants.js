e368e49021db31f3024fbef46e8d50f6184e4a37
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-25 17:25:10 -0700

    Add links for the lodging page to constants file

--- src/constants.js
+++ src/constants.js
@@ -16,15 +16,30 @@ export const pages = [
 export const BLANK_TARGET = '_blank'
 export const NEW_TAB = 'noopener noreferrer'
 
-// URLs
-export const NIKKI_PHONE = 'tel:4808654626'
+// About Links
 export const WILD_HORSE_RANCH_LINK = 'http://www.wldhorse.com/'
 export const WILD_HORSE_RANCH_ADDRESS_LINK = 'https://www.google.com/maps/place/The+Oasis+at+Wild+Horse+Ranch/@32.3305269,-111.1104973,17z/data=!3m1!4b1!4m5!3m4!1s0x86d674d400000001:0x6c507c274f08cc6b!8m2!3d32.3305269!4d-111.1083086'
+
+// FAQ Links
+export const NIKKI_PHONE = 'tel:4808654626'
+
+// Travel Links
 export const CHICAGO_TO_TUCSON_LINK = 'https://www.google.com/maps/dir/Chicago,+IL/6801+N.+Camino+Verde,+Tucson,+AZ+85743/@36.8586193,-103.8580443,6z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x880e2c3cd0f4cbed:0xafe0a6ad09c0c000!2m2!1d-87.6297982!2d41.8781136!1m5!1m1!1s0x86d5e0084a2620df:0x10d7e8ecaf363f89!2m2!1d-111.1083086!2d32.3305269'
 export const STL_TO_TUCSON_LINK = 'https://www.google.com/maps/dir/St.+Louis,+MO/6801+N.+Camino+Verde,+Tucson,+AZ+85743/@35.4376149,-105.1414636,6z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x87d8b4a9faed8ef9:0xbe39eaca22bbe05b!2m2!1d-90.1994042!2d38.6270025!1m5!1m1!1s0x86d5e0084a2620df:0x10d7e8ecaf363f89!2m2!1d-111.1083086!2d32.3305269'
 export const PHOENIX_SKY_HARBOR_LINK = 'https://www.skyharbor.com/'
 export const TUCSON_INTERNATIONAL_LINK = 'https://www.flytucson.com/'
 export const SKYSCANNER_LINK = 'https://www.skyscanner.com/'
+
+// Lodging Links
+export const HOLIDAY_INN_LINK = 'https://www.google.com/maps/dir//holiday+inn+express+tucson+north+marana/@32.358541,-111.1279018,13z/data=!3m1!5s0x86d60a9e0f314aa1:0x70ede6f901f381ab!4m8!4m7!1m0!1m5!1m1!1s0x86d60a9e0b4bd699:0x3afb12d66de80a5a!2m2!1d-111.0928824!2d32.3584733'
+export const HOLIDAY_INN_PHONE = 'tel:5205724777'
+export const COMFORT_INN_LINK = 'https://www.choicehotels.com/arizona/tucson/comfort-inn-hotels/az178?source=gyxt'
+export const COMFORT_INN_PHONE = 'tel:5205791099'
+export const LA_QUINTA_INN_LINK = 'https://www.wyndhamhotels.com/laquinta/tucson-arizona/la-quinta-nw-tucson-marana/overview?CID=LC:LQ::GGL:RIO:National:10989&iata=00093796'
+export const LA_QUINTA_INN_PHONE = 'tel:5205724235'
+export const AIR_BNB_LINK = 'https://www.airbnb.com/s/Tucson--AZ--United-States/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&date_picker_type=calendar&checkin=2021-10-07&checkout=2021-10-09&source=structured_search_input_header&search_type=unknown&ne_lat=32.41281321218237&ne_lng=-110.88226458930666&sw_lat=32.15943227786499&sw_lng=-111.23348376655275&zoom=12&search_by_map=true'
+
+// Points of Interest Links
 export const DESERT_MUSEUM_LINK = 'https://www.desertmuseum.org/'
 export const MOUNT_LEMMON_LINK = 'https://www.outdoorproject.com/united-states/arizona/mount-lemmons-windy-vista-point'
 export const EL_CHARROS_LINK = 'https://www.elcharrocafe.com/'
@@ -36,6 +51,8 @@ export const JAVELINA_CANTINA_LINK = 'http://www.javelinacantinasedona.com/'
 export const BELL_ROCK_LINK = 'https://www.tripadvisor.com/Attraction_Review-g31352-d126801-Reviews-Bell_Rock-Sedona_Arizona.html'
 export const SLIDE_ROCK_LINK = 'https://azstateparks.com/slide-rock/'
 export const JEEP_TOURS_LINK = 'https://www.sedona.net/jeep-tours'
+
+// Engagement Photos Links
 export const ENGAGEMENT_PHOTOS_LINK = 'https://ibb.co/album/S5cBHK'
 export const MPIX_LINK = 'https://www.mpix.com/products/prints'
 export const SARAH_LINK = 'https://www.sarahkellie.com/'
