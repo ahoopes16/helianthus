cdc660bbad1ea0c258858ebc35f816e5a3793870
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-25 18:31:39 -0700

    Add a NavLink to the logo image
    Now whenever a user clicks the logo, it will always take them
    to the About page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import logo from '../img/logo.png'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
     return (
         <div className='bg-navy text-parisienne text-5xl grid grid-cols-12'>
             <div className='flex items-center justify-center'>
-                <img className='h-20 w-20' src={logo} alt='Logo' />
+                <NavLink to='/about'>
+                    <img className='h-20 w-20' src={logo} alt='Logo' />
+                </NavLink>
             </div>
             <div className='col-span-11'>
                 <h1 className='text-center py-4'>Nikki &#38; Alex &#8226; October 8, 2021</h1>
