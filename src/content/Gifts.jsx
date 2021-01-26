34c8fbe83c5e0e9fce95b504fdd7623b5022239a
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-25 18:37:24 -0700

    Extract reused components into functional components within each page
    All of the pages are just different enough that it would be
    annoying to change each component enough if I extracted them all
    to their own files, but it does clean the markup if I extract them
    into components within their own files. That's good enough for me.

--- src/content/Gifts.jsx
+++ src/content/Gifts.jsx
@@ -2,22 +2,26 @@ import { Fragment } from 'react'
 import { ContentBody, ContentHeading } from '../components'
 
 function Gifts() {
+    const Section = ({ children }) => {
+        return <p className='mb-6'>{children}</p>
+    }
+
     return (
         <Fragment>
             <ContentHeading>Gifts</ContentHeading>
 
             <ContentBody>
-                <p className='mb-6'>
+                <Section>
                     You coming to our wedding is a <strong>huge</strong> gift!
                     We know that this is a far journey/destination wedding, and your attendance is more than enough.
-                </p>
+                </Section>
 
-                <p className='mb-6'>
+                <Section>
                     If we haven't convinced you, though, and you would still like to give a gift, we would love cash or gift cards!
                     We are currently saving up to buy a house, so cash is king.
                     Moreover, we are in a one bedroom apartment right now and have no location to store physical gifts for the time being.
                     We will not be registering anywhere.
-                </p>
+                </Section>
 
                 <p>Gift cards we would love:</p>
                 <ul className='ml-10 list-disc'>
