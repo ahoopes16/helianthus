1b8ccddc6f611472c74552f58906aa7ee765a31b
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-01-26 07:22:19 -0700

    Fire a sweetalert if the user is on mobile
    This is to let them know that their experience will be much
    better on a desktop computer or laptop for now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import './generated/tailwind.css'
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+import swal from 'sweetalert2'
 import { ContentCard, ContentWrapper, Header, Navigation } from './components'
-import { pages } from './constants'
+import { pages, TABLET_WIDTH } from './constants'
+
+function getViewportWidth() {
+  // Modern browsers
+  if (window.innerWidth) {
+    return window.innerWidth
+  }
+
+  // IE6
+  if (document.documentElement && document.documentElement.clientWidth) {
+    return document.documentElement.clientWidth
+  }
+
+  // Older versions of IE
+  return document.getElementsByTagName('body')[0].clientWidth
+}
 
 function App() {
+  if (getViewportWidth() <= TABLET_WIDTH) {
+    swal.fire({
+      title: 'Hey there!',
+      html: `<p>I see that you are on a tablet or mobile device!</p><br /><p>The site is not optimized for this yet. You'll have a better experience on a laptop or desktop computer. :)</p>`,
+      icon: 'info',
+      confirmButtonText: 'Got it!'
+    })
+  }
+
   return (
     <HashRouter>
       <div id='app' className='bg-steel text-gold'>
