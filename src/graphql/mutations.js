7a8deabf8010408128c6b034e6c71fa4c0622154
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-02-20 12:58:59 -0700

    Generate GraphQL API using amplify CLI

---
+++ src/graphql/mutations.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createGuest = /* GraphQL */ `
+  mutation CreateGuest(
+    $input: CreateGuestInput!
+    $condition: ModelGuestConditionInput
+  ) {
+    createGuest(input: $input, condition: $condition) {
+      id
+      firstName
+      lastName
+      group
+      plusOneAllowed
+      overTwentyOne
+      rsvp
+      rsvpDate
+      attendingRehearsalDinner
+      plusOneDinner
+      plusOneName
+      dinnerOption
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateGuest = /* GraphQL */ `
+  mutation UpdateGuest(
+    $input: UpdateGuestInput!
+    $condition: ModelGuestConditionInput
+  ) {
+    updateGuest(input: $input, condition: $condition) {
+      id
+      firstName
+      lastName
+      group
+      plusOneAllowed
+      overTwentyOne
+      rsvp
+      rsvpDate
+      attendingRehearsalDinner
+      plusOneDinner
+      plusOneName
+      dinnerOption
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteGuest = /* GraphQL */ `
+  mutation DeleteGuest(
+    $input: DeleteGuestInput!
+    $condition: ModelGuestConditionInput
+  ) {
+    deleteGuest(input: $input, condition: $condition) {
+      id
+      firstName
+      lastName
+      group
+      plusOneAllowed
+      overTwentyOne
+      rsvp
+      rsvpDate
+      attendingRehearsalDinner
+      plusOneDinner
+      plusOneName
+      dinnerOption
+      createdAt
+      updatedAt
+    }
+  }
+`;
