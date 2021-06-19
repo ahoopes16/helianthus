83375d4575d2d0823ba54949b6654498e36733ee
Alex Hoopes <kevin.alex.hoopes@gmail.com>
2021-06-19 16:46:19 -0700

    Turn off autocomplete and apply char limit on text input

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -6,7 +6,7 @@ function TextBox({ label, onChange, value, ...props }) {
     return (
         <label {...props} htmlFor={htmlName}>
             {label}:
-            <input className="ml-4 pl-1 border rounded" type="text" name={htmlName} value={value} onChange={onChange} />
+            <input className="ml-4 pl-1 border rounded" type="text" name={htmlName} value={value} onChange={onChange} autoComplete="off" maxLength="80" />
         </label>
     )
 }
